34e142b28031bd22a8260326eef14cffbfd4289a
CobbCoding <rircobb@gmail.com>
2023-10-29 19:01:04 -0400

    got some sort of parser working, I need to learn more about structs and classes in order to get it working with multiple expressions

--- main.cpp
+++ main.cpp
@@ -11,6 +11,26 @@ enum class TokenType{
     TYPE_MINUS,
 };
 
+enum class Operator{
+    TYPE_ADD,
+    TYPE_SUB,
+};
+
+string print_token_type(TokenType type){
+    switch(type){
+        case TokenType::TYPE_INT:
+            return "int";
+            break;
+        case TokenType::TYPE_PLUS:
+            return "plus";
+            break;
+        case TokenType::TYPE_MINUS:
+            return "minus";
+            break;
+    }
+    return "none";
+}
+
 struct Token{
     TokenType type;
     string text;
@@ -27,6 +47,75 @@ public:
             cout << tokens.front().text << endl;
             tokens.pop();
         }
+    }
+};
+
+class Node{
+public:
+    TokenType op;
+    string left;
+    string right;
+};
+
+class Parser{
+public:
+    Lexer lexer;
+    Node *tree;
+    Node *current_node;
+
+    Parser(){
+        tree = new Node;
+        current_node = tree;
+    }
+
+    void parse_lexer(){
+        while(!lexer.tokens.empty()){
+            Token current = get_next_token();
+            Token next;
+            switch(current.type){
+                case TokenType::TYPE_INT:
+                    next = get_next_token();
+                    if(!is_operator(next)){
+                        cerr << "error: expected operator" << endl;
+                        exit(1);
+                    }
+                    current_node->left = current.text;
+                    current_node->op = next.type;
+                    next = get_next_token();
+                    if(next.type != TokenType::TYPE_INT){
+                        cerr << "error: expected int" << endl;
+                        exit(1);
+                    }
+                    current_node->right = next.text;
+                    break;
+                case TokenType::TYPE_PLUS:
+                    current_node->op = current.type; 
+                    break;
+                case TokenType::TYPE_MINUS:
+                    current_node->op = current.type;
+                    break;
+            }
+        }
+        print_tree();
+    }
+private: 
+    void print_tree(){
+        current_node = tree;
+        cout << current_node->left << print_token_type(current_node->op) << current_node->right << endl;
+    }
+    bool is_operator(Token token){
+        if(token.type == TokenType::TYPE_PLUS || token.type == TokenType::TYPE_MINUS){
+            return true;
+        }
+        return false;
+    }
+    Token get_next_token(){
+        Token token = lexer.tokens.front();
+        lexer.tokens.pop();
+        return token;
+    }
+    Token peek_next_token(){
+        return lexer.tokens.front();
     }
 };
 
@@ -74,12 +163,10 @@ Lexer lex_file(string filename){
     return lexer;
 }
 
-void parse_lexer(Lexer lexer){
-    lexer.print_tokens();
-}
-
 int main(){
     Lexer lexer = lex_file("test.basic");
-    parse_lexer(lexer);
+    Parser parser;
+    parser.lexer = lexer;
+    parser.parse_lexer();
     return 0;
 }
